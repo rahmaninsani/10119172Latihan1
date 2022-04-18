120fef1aba001e92311800655dff295e08d88d27
Rahman Insani <rahmaninsani54@gmail.com>
2022-04-18 21:19:47 +0700

    Initial Commit

---
+++ app/src/main/java/com/rahmaninsani/a10119172latihan1/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+    NIM     : 10119172
+    Nama    : Rahman Insani
+    Kelas   : IF-5
+
+    Minggu, 17 April 2022
+*/
+
+package com.rahmaninsani.a10119172latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
